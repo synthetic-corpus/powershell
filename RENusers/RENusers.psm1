c2ca73deb304d2dc3c3b78ecd4039ba44adb2377
Joel Gonzaga <Joel@joelgonzaga.com>
2019-06-01 19:26:05 -0700

    added gitignore

--- RENusers/RENusers.psm1
+++ RENusers/RENusers.psm1
@@ -433,21 +433,34 @@ function new-RENaccounts {
         .DESCRIPTION
         This accepts properly formatted excel sheet, put it through a loop and 
         creates all the user accounts from it.
+
+        A properly formatted excel sheet will have the following as headers:
+            Name,Surname,Title,City,Department
+
+        All entries must be strings. The city must match one of the following:
+            Seattle, Los Angeles, Chicago
+        
+        Department must match the name of the correct OU relative to city *exactly* 
+        or it will fail input validation.
+
         .Parameter path
         A path to the correctly formatted .xls file
     #>
     [CMDletbinding()]
     Param(
-        [Parameter(mandatory=$true,Position=0,ValueFromPipeline=$true)]
-        [string]$path)
+        [Parameter(mandatory=$true,Position=0)]
+        [string]$path
 
     BEGIN {
-        # Import the the Excel sheet as objects, and validated it.
-        # If the first object does not have the exactly correct headers it fails.
+        $table = import-excel -path $path
     }
 
     PROCESS {
         # Run Through the Loop.
         # Out put an error, on each object, if any of the fields are blank.
+        foreach $row in $table{
+            write-verbose "Found that ",$row.name," is in the city of ",$row.city," and works as ",$row.title
+        }
+        
     }
 }
